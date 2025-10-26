850157db1dead5c3040550831f4029200c9f7126
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2025-10-26 16:39:06 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-VOICE_ID = "JBFqnCBsd6RMkjVDRZzb"  # तू चाहें तो customize कर सकता है
